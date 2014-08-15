bae9940525fc230a475593b7dadec27886e62255
Milo Yip <miloyip@gmail.com>
2014-08-15 20:07:24 +0800

    Implement strdup's statistics by strlen()

--- src/tests/strduptest.cpp
+++ src/tests/strduptest.cpp
@@ -18,6 +18,14 @@ public:
         return strdup((char*)userdata);
     }
 
+    virtual Stat Statistics(void* userdata) const {
+        Stat s;
+        memset(&s, 0, sizeof(s));
+        s.stringCount = 1;
+        s.stringLength = strlen((char*)userdata);
+        return s;
+    }
+
     virtual void Free(void* userdata) const {
         free(userdata);
     }
