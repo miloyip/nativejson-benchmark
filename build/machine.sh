eaa833d748bc692d6811aaa5ffa1a5abed5d5d78
miloyip <miloyip@gmail.com>
2015-04-19 11:55:08 +0800

    Fix machine name for OS X

--- build/machine.sh
+++ build/machine.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-grep -m 1 "model name" /proc/cpuinfo | sed 's/model name.*: //' | sed -r 's/\s+//g' | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/CPU//' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sysctl -n machdep.cpu.brand_string   | sed 's/model name.*: //' | sed -E 's/[ ]*//g' | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/CPU//' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
+else
+	grep -m 1 "model name" /proc/cpuinfo | sed 's/model name.*: //' | sed -r 's/\s+//g' | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/CPU//' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
+fi
 cat ../src/machine.h
