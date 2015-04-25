1ef47f77a8ade197f6e2b765be1b0d4668ec6725
Milo Yip <miloyip@gmail.com>
2015-04-25 15:30:26 +0800

    Fix RapidJSON Insitu Test on Windows

--- src/tests/rapidjsoninsitutest.cpp
+++ src/tests/rapidjsoninsitutest.cpp
@@ -2,5 +2,6 @@
 #define TEST_PARSE_FLAG kParseInsituFlag
 #define TEST_NAME "RapidJSON_Insitu (C++)"
 #define TEST_CLASS RapidjsonInsituTest
+#define RapidjsonParseResult RapidjsonParseInsituResult
 
 #include "rapidjsontest.cpp"
