6420836073fc5c19c7c08b0023238092ee33b765
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-08-25 20:59:09 -0700

    Fix compilation on OSX
    llvm-c++ doesn't seem to support `-Wl,--start-group` and
    `-Wl,--end-group`. I'm not sure how to get premake to stop
    generating this, so this was an easy hack to remove it.

--- build/premake.sh
+++ build/premake.sh
@@ -6,4 +6,6 @@ premake5 gmake
 #  https://github.com/miloyip/nativejson-benchmark/issues/25#issuecomment-174154308
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	sed -i.bak 's/,-x//' gmake/*.make
+	sed -i.bak 's/-Wl,--start-group//' gmake/*.make
+	sed -i.bak 's/-Wl,--end-group//' gmake/*.make
 fi
