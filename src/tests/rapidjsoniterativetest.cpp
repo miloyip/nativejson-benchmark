f9f3054c551cd9505c20b3c13676f1aeb85eb4be
StilesCrisis <johnstiles@John-Stiles-iMac.local>
2017-03-10 23:35:59 -0800

    Add RapidJSON_Iterative
    Iterative mode can have significantly different performance; worth
    adding it to the test suite.

---
+++ src/tests/rapidjsoniterativetest.cpp
@@ -0,0 +1,7 @@
+#define TEST_ITERATIVE
+#define TEST_PARSE_FLAG kParseIterativeFlag
+#define TEST_NAME "RapidJSON_Iterative (C++)"
+#define TEST_CLASS RapidjsonIterativeTest
+#define RapidjsonParseResult RapidjsonParseIterativeResult
+
+#include "rapidjsontest.cpp"
